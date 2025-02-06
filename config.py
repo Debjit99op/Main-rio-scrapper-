d3d03a5ad7aaf1d5ba903c09bf5c23a66394c448
Debjit99op <laand6486@gmail.com>
2025-02-07 01:05:17 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# Pyrogram setup
+API_ID = "26330824"  # Replace this API ID with your actual API ID
+API_HASH = "b555d42aaf84f5266a201abfbf5ef53f"  # Replace this API HASH with your actual API HASH
+SESSION_STRING = "BQGRxsgAdNdGKSS40mhd7zKacUzIFKB3UtJ1QV8bdevqkStIfbcr2ehxIrrqgpPKibNbyO20r2AF-vZexciZfr7z4OWnmVe96bAlc1P6dsRkxpqN7cHxz_gntq4_4e0o-o5o47xE8EfaqgP_dXk2OREt7h0Pg2oki_s6IeV9IJoCt17tGvkIPlOEyzLUt3-5_jFFDdQ8sZLJeBxmeck2ixazkDVlch50Cq9h8cnkpbQSqePzDyoFHyWfvpYTfD7p0tdXdZOF2yVfYs5t6esW50wVKWBBV7-PpmGHT6itynpwjawWsuH2Edwcft8s2TO_KSNl-xRdy0z1olD5JIcsm4cOHO71YAAAAAGYYWSZAA"  # Replace this SESSION STRING with your actual SESSION_STRING
+BOT_TOKEN = "7618455538:AAFDdUJ3xt-qgcyv6aGMYqiNcniZjxvCwfk"  # Replace this BOT_TOKEN
+
+# Admin IDs
+ADMIN_IDS = [7660546867, 8071612110]
+
+# Limits
+DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
+ADMIN_LIMIT = 50000 # Card Scrapping Limit For Admin
